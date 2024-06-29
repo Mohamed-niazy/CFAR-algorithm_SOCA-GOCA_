a3300b1bab2b0f9d5399a9d6e7e834475882448c
Mohamed-niazy <mohamedniazy972@gmail.com>
2024-06-29 16:04:56 +0300

    add screen shots

--- RTL_SV/top.sv
+++ RTL_SV/top.sv
@@ -4,17 +4,13 @@ module top ();
   /*declere clock signal*/
   /***********************/
   bit clk;
-
-
   /******************************************/
   /*generate clock signal  with period 10ns */
   /******************************************/
-
   always #5 clk = ~clk;
   /******************************************/
   /*  instantiate mudule and the interface  */
   /******************************************/
-
   detect_if inst_interface_detect_if (clk);
   cfar_algorithm inst_cfar_algorithm (inst_interface_detect_if.dut);
   cfar_tb inst_cfar_tb (inst_interface_detect_if.tb);
